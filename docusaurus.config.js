b2705e734e1337e2f98dcaed128c1d8fdefda454
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-05-16 21:56:55 +0900

    feat: 템플릿 메소드 패턴 포스팅 / update: 코드블록 하이라이트 언어 추가
    1. 템플릿 메소드 패턴 포스팅
    2. 코드블록 하이라이트 언어에 java를 추가 했습니다 (docusaurus.config.js)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,113 +1,112 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 module.exports = {
-  title: 'MFAM CS 스터디',
-  tagline: 'CS 기본 지식 공부를 위한 홈페이지입니다.',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
-      crossorigin: 'anonymous',
-    },
-  ],
-  themeConfig: {
-    navbar: {
-      title: 'CS 스터디',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: '시작하기',
-        },
-        {
-          href: 'https://github.com/MLifeFam/cs_interview',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: '시작하기',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: '참고 사이트',
-          items: [
-            {
-              label: 'Ready-For-Tech-Interview',
-              href: 'https://github.com/WooVictory/Ready-For-Tech-Interview',
-            },
-            {
-              label: 'Interview_Question_for_Beginner',
-              href:
-                'https://github.com/JaeYeopHan/Interview_Question_for_Beginner',
-            },
-            {
-              label: 'tech-interview-for-developer',
-              href: 'https://github.com/gyoogle/tech-interview-for-developer',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/MLifeFam/cs_interview',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} MFAM Project, Inc. Built with Docusaurus.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
+	title: "MFAM CS 스터디",
+	tagline: "CS 기본 지식 공부를 위한 홈페이지입니다.",
+	url: "https://your-docusaurus-test-site.com",
+	baseUrl: "/",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "img/favicon.ico",
+	organizationName: "facebook", // Usually your GitHub org/user name.
+	projectName: "docusaurus", // Usually your repo name.
+	stylesheets: [
+		{
+			href: "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css",
+			type: "text/css",
+			integrity: "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X",
+			crossorigin: "anonymous",
+		},
+	],
+	themeConfig: {
+		prism: {
+			additionalLanguages: ["java"],
+		},
+		navbar: {
+			title: "CS 스터디",
+			logo: {
+				alt: "My Site Logo",
+				src: "img/logo.svg",
+			},
+			items: [
+				{
+					type: "doc",
+					docId: "intro",
+					position: "left",
+					label: "시작하기",
+				},
+				{
+					href: "https://github.com/MLifeFam/cs_interview",
+					label: "GitHub",
+					position: "right",
+				},
+			],
+		},
+		footer: {
+			style: "dark",
+			links: [
+				{
+					title: "Docs",
+					items: [
+						{
+							label: "시작하기",
+							to: "/docs/intro",
+						},
+					],
+				},
+				{
+					title: "참고 사이트",
+					items: [
+						{
+							label: "Ready-For-Tech-Interview",
+							href: "https://github.com/WooVictory/Ready-For-Tech-Interview",
+						},
+						{
+							label: "Interview_Question_for_Beginner",
+							href: "https://github.com/JaeYeopHan/Interview_Question_for_Beginner",
+						},
+						{
+							label: "tech-interview-for-developer",
+							href: "https://github.com/gyoogle/tech-interview-for-developer",
+						},
+					],
+				},
+				{
+					title: "More",
+					items: [
+						{
+							label: "GitHub",
+							href: "https://github.com/MLifeFam/cs_interview",
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} MFAM Project, Inc. Built with Docusaurus.`,
+		},
+	},
+	presets: [
+		[
+			"@docusaurus/preset-classic",
+			{
+				docs: {
+					sidebarPath: require.resolve("./sidebars.js"),
+					// Please change this to your repo.
+					editUrl: "https://github.com/facebook/docusaurus/edit/master/website/",
+					remarkPlugins: [math],
+					rehypePlugins: [katex],
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					editUrl: "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+				},
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			},
+		],
+	],
 };
